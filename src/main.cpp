da4ca0deab4f98f96fce6efe771b27fb7607621a
aziula <117120748+aziula@users.noreply.github.com>
2023-12-10 02:58:24 -0500

    Cleaned up main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -95,28 +95,6 @@ GLuint CreateProgram(GLuint vs, GLuint fs)
     return shaderProgram;
 }
 
-struct Vertex
-{
-    Vector3 pos;
-    Vector3 col;
-};
-
-using Vertices = std::vector<Vertex>;
-
-// Returns a colour from a rainbow palette based on an index
-Vector3 RainbowColorGenerator(int index)
-{
-    static const std::vector<Vector3> colors = {
-        {1.0f, 0.0f, 0.0f}, // Red
-        {1.0f, 0.5f, 0.0f}, // Orange
-        {1.0f, 1.0f, 0.0f}, // Yellow
-        {0.0f, 1.0f, 0.0f}, // Green
-        {0.0f, 0.0f, 1.0f}, // Blue
-        {0.3f, 0.0f, 0.5f}, // Indigo
-        {0.5f, 0.0f, 0.5f}  // Violet
-    };
-    return colors[index % colors.size()];
-}
 
 
 int main()
@@ -153,82 +131,12 @@ int main()
     GLuint fsDefault = CreateShader(GL_FRAGMENT_SHADER, "./assets/shaders/Default.frag");
     GLuint shaderProgram = CreateProgram(vsDefault, fsDefault);
 
-    Vertices square1(4);
-    square1[0].pos = { -1.0f, 1.0f, 0.0f };
-    square1[1].pos = {  1.0f, 1.0f, 0.0f };
-    square1[2].pos = {  1.0f, -1.0f, 0.0f };
-    square1[3].pos = { -1.0f, -1.0f, 0.0f };
-    Vertices square2 = square1;
-    for (size_t i = 0; i < square1.size(); i++)
-    {
-        // Using Lerp(), find a point between the start and end points and set it as 'square2's position
-        Vector3 squareStartPoint = square1[i].pos;
-        Vector3 squareEndPoint = square1[(i + 1) % 4].pos;
-        square2[i].pos = Lerp(squareStartPoint, squareEndPoint, 0.5f);
-
-        // Assigns colors to the vertices of both squares
-        square1[i].col = RainbowColorGenerator(0);
-        square2[i].col = RainbowColorGenerator(1);
-    }
-
-    const size_t squareCount = 100;   // Number of squares to draw (changeable)
-    const size_t vertexCount = 4;   // Number of vertices in a square (DO NOT CHANGE)!
-    Vertices vertices(squareCount * vertexCount);
-
-    // TODO -- generate vertices (replace the memcpy's with a variable version of the above)
-    // memcpy(vertices.data(), square1.data(), sizeof Vertex * 4);
-    // memcpy(vertices.data() + 4, square2.data(), sizeof Vertex * 4);
-    memcpy(vertices.data(), square1.data(), sizeof(Vertex) * 4);
-
-    // Potential vertex-generation loop:
-    //for (size_t i = 0; i < squareCount * vertexCount; i += vertexCount)
-    //{
-    //    for (size_t j = 0; j < vertexCount; j++)
-    //    {
-    //        vertices[i + j] = previous_square_midpoints[j]
-    //        vertices[i + j] = random_colour
-    //    }
-    //}
-
-    // Vertex-generation loop
-    for (size_t i = 1; i < squareCount; i++)
-    {
-        for (size_t j = 0; j < vertexCount; j++)
-        {
-            // Get the starting and ending points of the square's edge
-            Vector3 squareStartPoint = vertices[(i - 1) * vertexCount + j].pos;
-            Vector3 squareEndPoint = vertices[(i - 1) * vertexCount + (j + 1) % vertexCount].pos;
-
-            // Finds an interpolated point along the edge, and its position changes 
-            float interpolationValue = 0.1f * (1.0f + sinf((float)(i + j) * 0.1f));
-            Vector3 interpolatedPoint = Lerp(squareStartPoint, squareEndPoint, interpolationValue);
-
-            // Assign the calculated position and color to the current vertex
-            vertices[i * vertexCount + j].pos = interpolatedPoint;
-            vertices[i * vertexCount + j].col = RainbowColorGenerator(static_cast<int>(i));
-        }
-    }
-
-
-    GLuint vaoLines, vboLines;
-    glGenVertexArrays(1, &vaoLines);
-    glGenBuffers(1, &vboLines);
-    glBindVertexArray(vaoLines);
-    glBindBuffer(GL_ARRAY_BUFFER, vboLines);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(Vertex) * vertices.size(), vertices.data(), GL_STATIC_DRAW);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 2 * SZ_FLOAT3, (void*)0);
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 2 * SZ_FLOAT3, (void*)(SZ_FLOAT3));
-    glEnableVertexAttribArray(0);
-    glEnableVertexAttribArray(1);
 
     glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
     glUseProgram(shaderProgram);
     while (!glfwWindowShouldClose(window))
     {
         glClear(GL_COLOR_BUFFER_BIT);
-        glBindVertexArray(vaoLines);
-        for (size_t i = 0; i < vertices.size(); i += 4)
-            glDrawArrays(GL_LINE_LOOP, i, 4);
 
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
         // -------------------------------------------------------------------------------
